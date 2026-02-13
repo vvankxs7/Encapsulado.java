54032d7a18de3b246c2e96223da5d7cd5927bda9
Cauê (vvankxs7) <cauecpereira06@gmail.com>
2026-02-13 15:03:18 -0300

    Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,17 @@
+public class Cliente{
+	private String nome;
+	private float credito;
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNome() {
+		return this.nome;
+	}
+	public void setCredito(float credito) {
+		this.credito = credito;
+	}
+	public float getCredito() {
+		return this.credito;
+	}
+}

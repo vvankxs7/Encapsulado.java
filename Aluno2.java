8ebd734945dd4c0b8a939ce7bce85b6a84cd38f8
Cauê (vvankxs7) <cauecpereira06@gmail.com>
2026-02-13 14:48:59 -0300

    Create Aluno2.java

---
+++ Aluno2.java
@@ -0,0 +1,17 @@
+public class Aluno2{
+	private String nome;
+	private float nota;
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNome() {
+		return this.nome;
+	}
+	public void setNota(float nota) {
+		this.nota = nota;
+	}
+	public float getNota() {
+		return this.nota;
+	}
+}
